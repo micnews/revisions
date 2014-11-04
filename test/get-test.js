eccce1162df482fb469150d99e0eb79adbfef86c
David Björklund <david.bjorklund@gmail.com>
2014-11-04 17:40:38 +0100

    remove tests that now exists in significant-stream

--- test/get-test.js
+++ test/get-test.js
@@ -3,7 +3,7 @@ var test = require('tape')
   , common = require('./common')
   , runTest = common.runGetTest
 
-test('get() none existing', function (t) {
+test('get() empty', function (t) {
   runTest('empty', [], function (err, revisions) {
     t.deepEqual(revisions, [])
     t.end(err)
@@ -15,97 +15,6 @@ test('get() one revision', function (t) {
 
   runTest('one-revision', inputs, function (err, revisions) {
     t.deepEqual(revisions, inputs)
-    t.end(err)
-  })
-})
-
-test('get() multiple compatible revisions', function (t) {
-  var inputs = [
-          { body: 'Hello', date: new Date(0) }
-        , { body: 'Hello, world', date: new Date(1000) }
-        , { body: 'Hello, world!', date: new Date(2000) }
-      ]
-
-  runTest('multiple-compatible', inputs, function (err, revisions) {
-    t.deepEqual(revisions, [ inputs[2] ])
-    t.end(err)
-  })
-})
-
-test('get() more complex, but compatible revisions', function (t) {
-  var inputs = [
-          { body: 'Hello', date: new Date(0) }
-        , { body: 'world!', date: new Date(1000) }
-        , { body: 'Hello, world!', date: new Date(2000)}
-      ]
-
-  runTest('multiple-compatible-complex', inputs, function (err, revisions) {
-    t.deepEqual(revisions, [ inputs[2] ])
-    t.end(err)
-  })
-})
-
-test('get() removing', function (t) {
-  var inputs = [
-          { body: 'Hello, world!', date: new Date(0) }
-        , { body: 'Hello', date: new Date(1000) }
-      ]
-
-  runTest('removing', inputs, function (err, revisions) {
-    t.deepEqual(revisions, inputs)
-    t.end(err)
-  })
-})
-
-test('get() multiple deletes', function (t) {
-  var inputs = [
-          { body: 'Hello, world!', date: new Date(0) }
-        , { body: 'Hello, world', date: new Date(1000) }
-        , { body: 'Hello', date: new Date(2000) }
-      ]
-
-  runTest('multiple-deletes', inputs, function (err, revisions) {
-    t.deepEqual(revisions, [ inputs[0], inputs[2] ])
-    t.end(err)
-  })
-})
-
-test('get() deletes & inserts', function (t) {
-  var inputs = [
-          { body: 'beep boop', date: new Date(0)}
-        , { body: 'Hello, world!', date: new Date(1000) }
-        , { body: 'Hello, world', date: new Date(2000) }
-        , { body: 'Hello', date: new Date(3000) }
-        , { body: 'Hello2', date: new Date(4000)}
-      ]
-
-  runTest('deletes-inserts', inputs, function (err, revisions) {
-    t.deepEqual(revisions, [ inputs[0], inputs[1], inputs[4] ])
-    t.end(err)
-  })
-})
-
-test('get() multiple ends with remove', function (t) {
-  var inputs = [
-          { body: 'Hello', date: new Date(0) }
-        , { body: 'Foo', date: new Date(1000) }
-        , { body: '', date: new Date(2000) }
-      ]
-
-  runTest('end-with-remove', inputs, function (err, revisions) {
-    t.deepEqual(revisions, inputs)
-    t.end(err)
-  })
-})
-
-test('get() unchanged', function (t) {
-  var inputs = [
-          { body: 'Hello', date: new Date(0) }
-        , { body: 'Hello', date: new Date(1000) }
-      ]
-
-  runTest('unchanged', inputs, function (err, revisions) {
-    t.deepEqual(revisions, [ inputs[1] ])
     t.end(err)
   })
 })
